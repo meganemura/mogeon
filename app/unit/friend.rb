829fce85ba2fb0628fbcb4659938b653f7a747aa
meganemura <mura2megane@gmail.com>
2013-12-14 22:34:21 +0900

    app: Add `Friend#random_move`

--- app/unit/friend.rb
+++ app/unit/friend.rb
@@ -5,5 +5,23 @@ module Mogeon
     SCALE = 1.0
     TEXTURE = "leatherarmor"
 
+    def random_move(callback)
+      x, y = Map.moving_amount(random_direction)
+      self.move(x, y, callback)
+    end
+
+    def random_direction
+      case rand(4)
+      when 0
+        :up
+      when 1
+        :right
+      when 2
+        :down
+      when 3
+        :left
+      end
+    end
+
   end
 end
