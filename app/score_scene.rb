7f393da4b0a16b2d2ad904d2784302c1ae30e919
meganemura <mura2megane@gmail.com>
2014-01-06 00:09:16 +0900

    app: Add background music to ScoreScene

--- app/score_scene.rb
+++ app/score_scene.rb
@@ -4,6 +4,14 @@ module Mogeon
     def initWithSize(size)
       super
 
+      # BGM
+      error = Pointer.new(:object)
+      background_music_url = NSBundle.mainBundle.URLForResource("music/score", withExtension: "mp3")
+      backgroundMusicPlayer = AVAudioPlayer.alloc.initWithContentsOfURL(background_music_url, error: error)
+      backgroundMusicPlayer.numberOfLoops = -1
+      backgroundMusicPlayer.prepareToPlay
+      backgroundMusicPlayer.play
+
       self.backgroundColor = SKColor.colorWithRed(1.0, green: 1.0, blue: 1.0, alpha: 1.0)
 
       # Label
@@ -19,6 +27,7 @@ module Mogeon
         SKAction.sequence([
           SKAction.waitForDuration(3.0 * SPEED),
           SKAction.runBlock(lambda {
+            backgroundMusicPlayer.stop
             reveal = SKTransition.flipHorizontalWithDuration(0.5 * SPEED)
             game_scene = GameScene.alloc.initWithSize(self.size)
             self.view.presentScene(game_scene, transition: reveal)
