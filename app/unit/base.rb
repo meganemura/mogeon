a6fa2850271f8ae73dac23f51df93e686f79d34b
meganemura <mura2megane@gmail.com>
2014-01-05 23:14:46 +0900

    app: Add `Unit::Base#forever`

--- app/unit/base.rb
+++ app/unit/base.rb
@@ -97,6 +97,12 @@ module Mogeon
         @actions = []
       end
 
+      def forever
+        sequence = SKAction.sequence(actions)
+        @actions = [SKAction.repeatActionForever(sequence)]
+        run_actions
+      end
+
       def stop_motion
         self.removeAllActions
         actions << SKAction.rotateToAngle(degrees_to_radians(0), duration: 0.1 * SPEED)
